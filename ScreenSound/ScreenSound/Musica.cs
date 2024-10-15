ba00fc60fa658aabd7f18e1a5a511719ce3af7de
Vitor Assis <vitorassis997@gmail.com>
2024-10-15 17:41:23 -0300

    projeto screensound

---
+++ ScreenSound/ScreenSound/Musica.cs
@@ -0,0 +1,23 @@
+﻿class Musica
+{
+    public string nome;
+    public string artista;
+    public int duracao;
+    public bool disponivel;
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome: {nome}");
+        Console.WriteLine($"Artista: {artista}");
+        Console.WriteLine($"Duracao: {duracao}");
+        Console.WriteLine($"Disponivel: {disponivel}");
+        if (disponivel)
+        {
+            Console.WriteLine("Disponivel no plano");
+        } else
+        {
+            Console.WriteLine("Adquita o plano Plus+");
+        }
+    }
+}
+ 
