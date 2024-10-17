59153370407f254f68c66cb3182b83eae80f3cd5
Vitor Assis <vitorassis997@gmail.com>
2024-10-17 15:22:12 -0300

    continuando curso

--- ScreenSound/ScreenSound/Musica.cs
+++ ScreenSound/ScreenSound/Musica.cs
@@ -1,17 +1,19 @@
 ﻿class Musica
 {
-    public string nome;
-    public string artista;
-    public int duracao;
-    public bool disponivel;
+    public string Nome { get; set; }
+    public string Artista { get; set; }
+    public int Duracao { get; set; }
+    public bool Disponivel { get;  set; } //operacao de leitura e escrita 
+    public string DescricaoResumida =>
+        $"A musica {Nome} pertence a banda {Artista}";
 
     public void ExibirFichaTecnica()
     {
-        Console.WriteLine($"Nome: {nome}");
-        Console.WriteLine($"Artista: {artista}");
-        Console.WriteLine($"Duracao: {duracao}");
-        Console.WriteLine($"Disponivel: {disponivel}");
-        if (disponivel)
+        Console.WriteLine($"Nome: {Nome}");
+        Console.WriteLine($"Artista: {Artista}");
+        Console.WriteLine($"Duracao: {Duracao}");
+        Console.WriteLine($"Disponivel: {Disponivel}");
+        if (Disponivel)
         {
             Console.WriteLine("Disponivel no plano");
         } else
